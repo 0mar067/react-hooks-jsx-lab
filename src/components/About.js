e344cbc4bb66d78e0f94115b84dfac79cb43db69
elnabaseugine <elnabaseugine@gmail.com>
2025-07-12 21:43:43 +0300

    basic jsx syntax

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="About">
+    <h2>About Me</h2>
+    <p>My name is Omar and I am a software engineer.</p>
+    <img src={image} alt="i made this" />
+  </div>;
 }
 
 export default About;
